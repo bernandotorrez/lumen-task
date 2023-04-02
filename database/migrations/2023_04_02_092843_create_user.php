6620d35abd85d12684cc40c854fee62f8743f2d2
bernandotorrez <bernandotorrez4@gmail.com>
2023-04-02 17:07:12 +0700

    first commit

---
+++ database/migrations/2023_04_02_092843_create_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id('id_user');
+            $table->string('username', 50)->nullable(false);
+            $table->string('password', 50)->nullable(false);
+            $table->string('name', 100)->nullable(false);
+            $table->enum('is_deleted', ['0', '1'])->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
---
+++ database/migrations/2023_04_02_093129_create_task.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTask extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id('id_task');
+            $table->bigInteger('id_user');
+            $table->string('task_name', 50)->nullable(false);
+            $table->enum('status', ['0', '1', '2'])->default('0')->comment('0 = Pending, 1 = Active, 2 = Done');
+            $table->enum('is_deleted', ['0', '1'])->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
